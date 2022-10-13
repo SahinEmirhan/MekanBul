903bfe0b9dd0d4844694c1e93cdb918d717d3673
Emirhan Şahin <sahnemirhan@gmail.com>
2022-10-13 23:10:06 +0300

    second commit

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const anaSayfa=function(req, res, next)
+  {
+    res.render('anaSayfa', { title: 'Ana Sayfa' });
+  }
+  const mekanBilgisi=function(req, res, next)
+  {
+    res.render('mekanBilgisi', { title: 'Mekan Bilgisi' });
+  }
+  const yorumEkle=function(req, res, next)
+  {
+    res.render('yorumEkle', { title: 'Yorum ekle' });
+  }
+
+  
+
+
+  module.exports = 
+  {
+    anaSayfa,mekanBilgisi,yorumEkle
+  }
